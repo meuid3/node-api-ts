e8af4d54115484a1e80a17d272ab5fa76d836e73
Marcos Moraes Pereira <marcosmpereira1992@gmail.com>
2021-05-06 23:56:04 -0300

    [UPDATE]- Adicionado métodos de crud na camada de controle de usuário

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -3,10 +3,16 @@ import UsuarioBO from '../business/UsuarioBO';
 
 class UsuarioController {
 
-  public async criarUsuario( request: Request, response : Response ) {
+  public async create( request: Request, response : Response ) {
     const usuarioBO = new UsuarioBO();
-    const create =  await usuarioBO.criarUsuario(request.body);
+    const create =  await usuarioBO.create(request.body);
     return response.json(create);
+  }
+
+  public async update( request: Request, response: Response) {
+    const usuarioBO = new UsuarioBO();
+    const user = await usuarioBO.update(request.body);
+    return response.json(user);
   }
 
   public async findAll( request: Request, response: Response) {
