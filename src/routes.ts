f8976c55c80221471518aaa4013d2bc4358c7422
Marcos Moraes Pereira <marcosmpereira1992@gmail.com>
2021-05-06 23:41:13 -0300

    [UPDATE] - Adicionado nova rota de alteração de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,9 @@ import  UsuarioController from './controllers/UsuarioController';
 
 const routes = express.Router();
 
-routes.post('/usuario', UsuarioController.criarUsuario);
+
+routes.post('/usuario', UsuarioController.create);
+routes.post('/usuario/:id/alterar', UsuarioController.update);
 routes.get('/usuario', UsuarioController.findAll);
 
 export default routes;
